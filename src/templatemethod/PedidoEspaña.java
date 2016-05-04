fdcb19c77ec21c7a45bd177d9aa8861f0c72338a
Adrian Vergara <adrianvergara22@gmail.com>
2016-05-04 00:17:44 -0500

    TemplateMethod

---
+++ src/templatemethod/PedidoEspaña.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templatemethod;
+
+/**
+ *
+ * @author Adrian
+ */
+public class PedidoEspaña extends Pedido{
+
+    @Override
+    protected void calculaIVA() {
+        importeIVA = importeSinIVA * 0.21;
+    }
+    
+}
